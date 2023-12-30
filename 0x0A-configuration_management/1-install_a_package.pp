0dc979d52b26424e9e52ad0ff66dd70ce3bcddff
EricoMartin <ericomartin.0785@gmail.com>
2023-12-30 05:57:21 -0800

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
 # This is a puppet script that installs flask from pip3
 
-exec {'install_flask':
-  command     => '/usr/bin/pip3 install Flask==2.1.0',
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
