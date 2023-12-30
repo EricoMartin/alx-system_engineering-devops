4bf75731ac884dd6223ba601ecc79cb99ac19fff
EricoMartin <ericomartin.0785@gmail.com>
2023-12-30 08:29:04 -0800

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,14 @@
-#!/usr/bin/pip
+# Install werkzeug using pip3
+
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip',
+}
 
 # This is a puppet script that installs flask from pip3
 
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
+  require  => Package['Werkzeug'], # werkzeug is installed first
 }
