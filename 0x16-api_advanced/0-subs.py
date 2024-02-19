b0b20356e319e48c38e0dea2b55753490dbb8dc4
EricoMartin <ericomartin.0785@gmail.com>
2024-02-19 12:01:50 -0800

    API Advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,12 +10,16 @@ def number_of_subscribers(subreddit):
     '''
         returns the number of subscribers for a given subreddit
     '''
-    url = "https://www.reddit.com/r/{}/about".format(subreddit)
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    if response.status_code == 200:
+        try:
+            results = response.json().get("data")
+            return results.get("subscribers")
+        except json.decoder.JSONDecodeError as e:
+            print(f"JSON Decode Error: {e}")
+    else:
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
