a8b37d3ac734476e1daa81607930f68210183192
EricoMartin <ericomartin.0785@gmail.com>
2024-02-19 12:23:34 -0800

    API Advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,7 +12,7 @@ def number_of_subscribers(subreddit):
     '''
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7)"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
