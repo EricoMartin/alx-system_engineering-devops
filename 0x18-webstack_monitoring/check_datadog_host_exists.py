7eb820c1e7baccfd9f4f3d420aba56d2258fcb83
EricoMartin <ericomartin.0785@gmail.com>
2024-02-20 04:30:19 -0800

    0x18. Webstack monitoring

---
+++ 0x18-webstack_monitoring/check_datadog_host_exists.py
@@ -0,0 +1,4 @@
+export API_KEY="6362f82498f25fdcad65eff3421f0a03"
+export HOSTNAME="ca60d9a08080748716f1fed673915947a78ba8d4"
+
+curl -X GET "https://api.datadoghq.com/api/v1/hosts/${HOSTNAME}?api_key=${API_KEY}" -H "Content-Type: application/json"
